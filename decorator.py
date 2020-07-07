2873a38eebdce9f1057d857847d0ea1fc888e91a
sam001386 <32662528+sam001386@users.noreply.github.com>
2020-07-06 21:56:38 -0400

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -18,3 +18,23 @@ def slow_method():
     print("Done!")
     
 slow_method()
+
+
+######
+class Mycache(object):
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    
+    def __call__(self, *args):
+        if not args in self.cache:
+            self.cache[args] = self.func(*args)
+        return self.cache[args]
+
+@Mycache
+def fib(n):
+    if n <= 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)  
+
+print(fib(400))

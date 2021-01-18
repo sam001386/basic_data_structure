8eb0a9d4c4ee07d23cb32140daf9fd74b32cb621
sam001386 <32662528+sam001386@users.noreply.github.com>
2021-01-18 16:05:34 -0500

    Update min_spanning_tree.py

--- min_spanning_tree.py
+++ min_spanning_tree.py
@@ -1,4 +1,34 @@
 
+# LC 1584
+
+# Prim approach
+class Solution(object):
+    def minCostConnectPoints(self, A):
+        seen = set()
+        # 这里假设初始的节点是0
+        seen.add(0)
+        # 当前节点
+        cur = 0
+        res = 0
+        pq = []
+        while len(seen) < len(A):
+            for nxt in range(len(A)):
+                # 如果“下一个节点”已经被访问过了，或者等于当前节点则跳过
+                if cur == nxt or nxt in seen:
+                    continue
+                # 这里使用heap（priority queue）来实现找到一条新的边
+                else:
+                    heapq.heappush(pq, (abs(A[cur][0] - A[nxt][0]) + abs(A[cur][1] - A[nxt][1]), nxt))
+            # 如果当前节点已经被访问，则从heap（priority queue）弹出
+            while pq[0][1] in seen:
+                heapq.heappop(pq)
+            res += pq[0][0]
+            cur = pq[0][1]
+            heapq.heappop(pq)
+            # 记录下当前节点已经被访问过了
+            seen.add(cur)
+
+        return res
 
 # Kruskal approach
 class Solution(object):
